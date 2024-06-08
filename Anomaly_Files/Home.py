c4638638989ff35dd46aff511d8c16ac95c7471c
Joellots <133900071+Joellots@users.noreply.github.com>
2024-06-08 18:12:18 +0300

    Update Home.py

--- Anomaly_Files/Home.py
+++ Anomaly_Files/Home.py
@@ -26,7 +26,7 @@ config_path = os.path.join(current_dir, 'config.yaml')
 
 cred = credentials.Certificate(os.path.join(current_dir, 'diplom-site-1adda-ad818bf284ff.json'))
 
-db = firestore.client()
+#db = firestore.client()
 
 saved_creds = doc_ref = db.collection('credentials')
 print(saved_creds)
