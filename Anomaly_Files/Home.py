2dfaff2bead28c5a328d7b7d85c8779b55f740f4
Joellots <133900071+Joellots@users.noreply.github.com>
2024-06-08 22:42:49 +0300

    Update Home.py

--- Anomaly_Files/Home.py
+++ Anomaly_Files/Home.py
@@ -98,7 +98,7 @@ if authentication_status == None:
             letters = string.ascii_letters  # This includes both lowercase and uppercase letters
             random_password = ''.join(random.choice(letters) for i in range(10))
 
-            db_new = db.collection('credentials').document('username').collection(username_of_registered_user).document(username_of_registered_user)
+            db_new = db.collection('credentials').document('usernames').collection(username_of_registered_user).document(username_of_registered_user)
            
             #doc_ref = db.collection('credentials').document('usernames').collection(username_of_registered_user)
            
