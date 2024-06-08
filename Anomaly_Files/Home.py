99b9d7c7c5f5fb59cd5bd67b03d5dbd70f31301c
Joellots <133900071+Joellots@users.noreply.github.com>
2024-06-08 21:41:12 +0300

    Update Home.py

--- Anomaly_Files/Home.py
+++ Anomaly_Files/Home.py
@@ -99,7 +99,7 @@ if authentication_status == None:
             random_password = ''.join(random.choice(letters) for i in range(10))
 
             doc_ref = db.collection('credentials').document('usernames').collection(username_of_forgotten_password)
-            doc_ref.set({
+            doc_ref.add({
                 'name': name_of_registered_user,
                 'email': email_of_registered_user,
                 'password': random_password,
