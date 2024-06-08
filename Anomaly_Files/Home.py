ef2c8d0a958950c6fe91230c7ce6daaa09ad1722
Joellots <133900071+Joellots@users.noreply.github.com>
2024-06-08 22:02:10 +0300

    Update Home.py

--- Anomaly_Files/Home.py
+++ Anomaly_Files/Home.py
@@ -98,10 +98,13 @@ if authentication_status == None:
             letters = string.ascii_letters  # This includes both lowercase and uppercase letters
             random_password = ''.join(random.choice(letters) for i in range(10))
 
+            db_new = db = firestore.client()
+            db_new.collection('credentials').document('usernames').collection(collection_name)
+
             
-            doc_ref = db.collection('credentials').document('usernames').collection(username_of_forgotten_password)
-            st.write(doc_ref)
-            doc_ref.document().set({
+            #doc_ref = db.collection('credentials').document('usernames').collection(username_of_forgotten_password)
+            st.write(db_new)
+            db_new.document().set({
                 'name': name_of_registered_user,
                 'email': email_of_registered_user,
                 'password': random_password,
