54c5f9b01eb36b1a3f5006920c248cc4b6c505fe
Joellots <133900071+Joellots@users.noreply.github.com>
2024-06-10 03:04:40 +0300

    Update Home.py

--- Anomaly_Files/Home.py
+++ Anomaly_Files/Home.py
@@ -106,8 +106,6 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(current_dir, 'config.yaml')
 st.logo(os.path.join(current_dir, 'knrtu_logo.png'), link=None)
 
-import os.path.join(current_dir, "col_definition.py") as cd
-
 cred_path = os.path.join(current_dir, 'anomaly-detection-d4b91-firebase-adminsdk-lwlgg-d92f4bd41c.json')
 db = firestore.Client.from_service_account_json(cred_path)
 
