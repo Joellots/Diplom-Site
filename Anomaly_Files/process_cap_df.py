9a1c120a4778dea4554b514684fae06f9a9c69f8
Joellots <133900071+Joellots@users.noreply.github.com>
2024-06-18 08:59:31 +0300

    Update process_cap_df.py

--- Anomaly_Files/process_cap_df.py
+++ Anomaly_Files/process_cap_df.py
@@ -1,96 +1,126 @@
-
 import pandas as pd
 
 def compute_metrics(df):
-    df['dst_host_srv_count'] = df.apply(lambda row: compute_dst_host_srv_count(df, row['tcp.dstport']), axis=1)
-    df['dst_host_same_srv_rate'] = df.apply(lambda row: compute_dst_host_same_srv_rate(df, row['tcp.dstport']), axis=1)
-    df['dst_host_srv_diff_host_rate'] = df.apply(lambda row: compute_dst_host_srv_diff_host_rate(df, row['tcp.dstport']), axis=1)
-    df['dst_host_srv_serror_rate'] = df.apply(lambda row: compute_dst_host_srv_serror_rate(df, row['tcp.dstport']), axis=1)
-    df['count'] = df.apply(lambda row: compute_count(df, row['tcp.dstport']), axis=1)
-    df['diff_srv_rate'] = df.apply(lambda row: compute_diff_srv_rate(df, row['tcp.srcport']), axis=1) 
-    df['dst_host_serror_rate'] = df.apply(lambda row: compute_dst_host_serror_rate(df, row['tcp.dstport']), axis=1)
-    df['same_srv_rate'] = df.apply(lambda row: compute_same_srv_rate(df, row['tcp.srcport']), axis=1)
-    df['dst_host_diff_srv_rate'] = df.apply(lambda row: compute_dst_host_diff_srv_rate(df, row['tcp.dstport']), axis=1)
-    df['src_bytes'] = df['ip.len']
-    df['dst_bytes'] = df['tcp.urgent_pointer']    
-    df['dst_host_rerror_rate'] = df.apply(lambda row: compute_dst_host_rerror_rate(df, row['tcp.dstport']), axis=1)
-  
-    return df
+    clean_df = pd.DataFrame()
 
-# Define helper functions for metrics calculation
-def compute_same_srv_rate(df, src_port):
-    same_srv_connections = df[df['tcp.srcport'] == src_port]
-    total_connections = len(df)
-    if total_connections > 0:
-        return len(same_srv_connections) / total_connections
-    else:
+    # Calculate metrics specific to each packet
+    clean_df['dst_host_srv_count'] = df.apply(lambda x: compute_dst_host_srv_count(df, x), axis=1)
+    clean_df['dst_host_same_srv_rate'] = df.apply(lambda x: compute_dst_host_same_srv_rate(df, x), axis=1)
+    clean_df['dst_host_srv_diff_host_rate'] = df.apply(lambda x: compute_dst_host_srv_diff_host_rate(df, x), axis=1)
+    clean_df['dst_host_srv_serror_rate'] = df.apply(lambda x: compute_dst_host_srv_serror_rate(df, x), axis=1)
+    clean_df['count'] = df.apply(lambda x: compute_count(df, x), axis=1)
+    clean_df['diff_srv_rate'] = df.apply(lambda x: compute_diff_srv_rate(df, x), axis=1)
+    clean_df['dst_host_serror_rate'] = df.apply(lambda x: compute_dst_host_serror_rate(df, x), axis=1)
+    clean_df['same_srv_rate'] = df.apply(lambda x: compute_same_srv_rate(df, x), axis=1)
+    clean_df['dst_host_diff_srv_rate'] = df.apply(lambda x: compute_dst_host_diff_srv_rate(df, x), axis=1)
+    clean_df['src_bytes'] = df['ip.len']
+    clean_df['dst_bytes'] = df['tcp.urgent_pointer']
+    clean_df['dst_host_rerror_rate'] = df.apply(lambda x: compute_dst_host_rerror_rate(df, x), axis=1)
+    clean_df['flag'] = df['tcp.flags.mapped']
+    
+    # Write to CSV for reference
+    #clean_df.to_csv('OUTPUT.csv', index=False)
+    
+    return clean_df
+
+# Helper functions for metrics calculation
+
+def compute_dst_host_srv_count(df, row):
+    dst_host_srv_count = df[df['tcp.dstport'] == row['tcp.dstport']]['tcp.srcport'].nunique()
+    return dst_host_srv_count
+
+def compute_dst_host_same_srv_rate(df, row):
+    group = df[df['ip.dst'] == row['ip.dst']]
+    dst_host_count = len(group)
+    if dst_host_count == 0:
         return 0.0
+    
+    max_same_srv_count = group[group['tcp.dstport'] == row['tcp.dstport']].shape[0]
+    dst_host_same_srv_rate = max_same_srv_count / dst_host_count
+    return dst_host_same_srv_rate
 
-def compute_diff_srv_rate(df, src_port):
-    diff_srv_connections = df[df['tcp.srcport'] != src_port]
-    total_connections = len(df)
-    if total_connections > 0:
-        return len(diff_srv_connections) / total_connections
-    else:
+def compute_dst_host_srv_diff_host_rate(df, row):
+    group = df[df['tcp.dstport'] == row['tcp.dstport']]
+    dst_host_srv_count = len(group)
+    if dst_host_srv_count == 0:
         return 0.0
+    
+    unique_dst_ips = group['ip.dst'].nunique()
+    dst_host_srv_diff_host_rate = (dst_host_srv_count - unique_dst_ips) / dst_host_srv_count
+    return dst_host_srv_diff_host_rate
 
-def compute_dst_host_same_srv_rate(df, dst_port):
-    same_srv_connections = df[df['tcp.dstport'] == dst_port]
+def compute_dst_host_srv_serror_rate(df, row):
     total_connections = len(df)
-    if total_connections > 0:
-        return len(same_srv_connections) / total_connections
-    else:
+    if total_connections == 0:
         return 0.0
+    
+    serror_count = df['tcp.flags.mapped'].isin(['S0', 'S1', 'S2', 'S3']).sum()
+    dst_host_srv_serror_rate = serror_count / total_connections
+    return dst_host_srv_serror_rate
 
-def compute_dst_host_srv_diff_host_rate(df, dst_port):
-    diff_srv_connections = df[df['tcp.dstport'] != dst_port]
+def compute_count(df, row):
+    df_sorted = df[df['frame.time_relative'] <= row['frame.time_relative']]
+    count = 0
+    current_time = row['frame.time_relative']
+    current_dst_host = row['ip.dst']
+    
+    for j, prev_row in df_sorted.iterrows():
+        prev_time = prev_row['frame.time_relative']
+        prev_dst_host = prev_row['ip.dst']
+        
+        if current_time - prev_time > 2.0:
+            break
+        
+        if current_dst_host == prev_dst_host:
+            count += 1
+            
+    return count
+
+def compute_diff_srv_rate(df, row):
     total_connections = len(df)
-    if total_connections > 0:
-        return len(diff_srv_connections) / total_connections
-    else:
+    if total_connections == 0:
         return 0.0
+    
+    unique_services = df['tcp.dstport'].nunique()
+    diff_srv_count = total_connections - unique_services
+    diff_srv_rate = diff_srv_count / total_connections
+    return diff_srv_rate
 
-def compute_dst_host_srv_count(df, dst_port):
-    srv_count = len(df[df['tcp.dstport'] == dst_port])
-    return srv_count
+def compute_dst_host_serror_rate(df, row):
+    group = df[df['ip.dst'] == row['ip.dst']]
+    total_connections = len(group)
+    if total_connections == 0:
+        return 0.0
+    
+    serror_count = (group['tcp.flags.mapped'].isin(['S0', 'S1', 'S2', 'S3'])).sum()
+    dst_host_serror_rate = serror_count / total_connections
+    return dst_host_serror_rate
 
-def compute_dst_host_diff_srv_rate(df, dst_port):
+def compute_same_srv_rate(df, row):
     total_connections = len(df)
-    dst_host_count = len(df[df['tcp.dstport'] == dst_port])
-    if total_connections > 0:
-        return (total_connections - dst_host_count) / total_connections
-    else:
+    if total_connections == 0:
         return 0.0
+    
+    same_srv_count = df[df['tcp.dstport'] == row['tcp.dstport']].shape[0]
+    same_srv_rate = same_srv_count / total_connections
+    return same_srv_rate
 
-def compute_dst_host_srv_serror_rate(df, dst_port):
-    serror_connections = df[(df['tcp.dstport'] == dst_port) & (df['tcp.flags.syn'] == True)]
-    total_connections = len(df)
-    if total_connections > 0:
-        return len(serror_connections) / total_connections
-    else:
+def compute_dst_host_diff_srv_rate(df, row):
+    group = df[df['ip.dst'] == row['ip.dst']]
+    total_connections = len(group)
+    if total_connections == 0:
         return 0.0
+    
+    unique_services = group['tcp.dstport'].nunique()
+    diff_srv_rate = (total_connections - unique_services) / total_connections
+    return diff_srv_rate
 
-def compute_dst_host_rerror_rate(df, dst_port):
-    rerror_connections = df[(df['tcp.dstport'] == dst_port) & (df['tcp.flags.reset'] == True)]
-    total_connections = len(df)
-    if total_connections > 0:
-        return len(rerror_connections) / total_connections
-    else:
+def compute_dst_host_rerror_rate(df, row):
+    group = df[df['ip.dst'] == row['ip.dst']]
+    total_connections = len(group)
+    if total_connections == 0:
         return 0.0
-
-def compute_dst_host_serror_rate(df, dst_port):
-    serror_connections = df[(df['tcp.dstport'] == dst_port) & (df['tcp.flags.syn'] == True)]
-    total_connections = len(df)
-    if total_connections > 0:
-        return len(serror_connections) / total_connections
-    else:
-        return 0.0
-
-def compute_count(df, dst_port):
-    count_connections = len(df[df['tcp.dstport'] == dst_port])
-    return count_connections
-
-
-def process_packet_capture_data(df):
-    df_with_metrics = compute_metrics(df)
-    return df_with_metrics
+    
+    rerror_count = (group['tcp.flags.mapped'] == 'REJ').sum()
+    rerror_rate = rerror_count / total_connections
+    return rerror_rate
